2dbc8bd452b8a448755999bfca2297f70b657316
Ячменева Екатерина Викторовна <e_yachmeneva@igirgi.su>
2023-08-09 15:46:36 +0300

    modaloverlay and modal fixed

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -8,41 +8,31 @@ import ReactDOM from "react-dom";
 
 const modalRoot = document.getElementById("react-modals");
 
-
 export default function Modal ( { children, isOpen, onClose } ) {
-   
-  const closeModal = () => {
+  const closeWithEscape = (ev) => {
+    if (ev.key === "Escape") {
         onClose();
-      };
-
-    
+    } 
+  } 
     useEffect(() => {
-      const closeWithEscape = (ev) => {
-        if (ev.key === "Escape"){
-            onClose();
-        } 
-    }
-      if(isOpen){
         document.addEventListener("keydown", closeWithEscape);
         return () => {
           document.removeEventListener ("keydown", closeWithEscape);
-      }
+      
     }
-    },[isOpen]);
+    },[onClose]);
         // Возвращаем ReactDOM.createPortal, 
         // который поместит дочерние элементы в modalRoot
     
     return ReactDOM.createPortal(
                 <>
-                   <ModalOverlay oncloseModal={onClose} />
-                    <div className={`${styles.container} p-10`}>
-        <div className={styles.header}>
-             <button className={styles.header__button} onClick={closeModal}>
+                 <div className={`${styles.container} p-10`}>
+                   <button className={styles.header__button} onClick={onClose}>
             <CloseIcon type="primary" />
-          </button>
-        </div>
-        <div className={styles.content}>{children}</div>
-      </div>
+              </button>
+              <div className={styles.content}>{children}</div>
+              </div>
+            <ModalOverlay onCloseModal={onClose} />
                 </>, 
             modalRoot
         );
--- src/components/ModalOverlay/ModalOverlay.jsx
+++ src/components/ModalOverlay/ModalOverlay.jsx
@@ -1,10 +1,14 @@
 import styles from './modal-overlay.module.css';
 import PropTypes from 'prop-types';
 
-export default function ModalOverlay({oncloseModal}) {
-  return <div className={styles.overlay} onClick={oncloseModal}></div>;
+export default function ModalOverlay({onCloseModal}) {
+  return (
+    <div
+    className={styles.overlay} 
+    onClick={e => (e.currentTarget === e.target) && onCloseModal()}
+ />
+  )
 }
-
 ModalOverlay.propTypes = {
     oncloseModal: PropTypes.func.isRequired,
   };
