51f89ef4b66686d5d6af0ed9371e1afc0f86b6bb
Ячменева Екатерина Викторовна <e_yachmeneva@igirgi.su>
2023-09-19 17:18:23 +0300

    form added

---
+++ src/components/Form/Form.js
@@ -0,0 +1,34 @@
+import styles from "./form.module.css";
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { linkType } from "../../utils/types";
+
+function Form({ children, title, buttonText, links, onFormSubmit }) {
+  return (
+    <form className={styles.form} onSubmit={onFormSubmit}>
+      <h1 className="text text_type_main-medium mb-6">{title}</h1>
+      {children}
+      <Button htmlType="submit" type="primary" size="medium" extraClass="mb-20">
+        {buttonText}
+      </Button>
+      {links.map((link, index) => (
+        <p className="text text_color_inactive mb-4" key={index}>
+          {link.title}
+          <Link to={link.path} className={`${styles.link} ml-2`}>
+            {link.linkName}
+          </Link>
+        </p>
+      ))}
+    </form>
+  );
+}
+Form.propTypes = {
+  title: PropTypes.string,
+  buttonText: PropTypes.string,
+  onFormSubmit: PropTypes.func,
+  links: PropTypes.arrayOf(linkType),
+  children: PropTypes.node,
+};
+
+export default Form;
